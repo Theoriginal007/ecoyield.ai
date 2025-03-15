c53f57cf02af13c82fcef3fe26ecad72adf55cba
Theoriginal007 <lauragachanja007@gmail.com>
2025-03-15 14:53:57 +0300

    changes

--- CROP-RECOMMENDATION/webapp.py
+++ CROP-RECOMMENDATION/webapp.py
@@ -73,7 +73,7 @@ def main():
     # # Setting the title of the web app
     st.markdown("<h1 style='text-align: center;'>SMART CROP RECOMMENDATIONS", unsafe_allow_html=True)
     
-    st.sidebar.title("HarvestHaven")
+    st.sidebar.title("EcoYield AI")
     # # Input fields for the user to enter the environmental factors
     st.sidebar.header("Enter Crop Details")
     nitrogen = st.sidebar.number_input("Nitrogen", min_value=0.0, max_value=140.0, value=0.0, step=0.1)
